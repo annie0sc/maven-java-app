985cecd5e78dcd858ceee1e9e60055c2ed8a2314
annie0sc <annie0sc@gmail.com>
2021-02-19 15:32:35 -0600

    added maven

---
+++ src/test/java/edu/nwmsu/bigdata/annie/AppTest.java
@@ -0,0 +1,20 @@
+package edu.nwmsu.bigdata.annie;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
